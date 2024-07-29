56b34200e2dcbb9ca4d6abc5d6c1f0663ceb1a3a
Bob Oyier <boboyier92@gmail.com>
2024-07-29 08:59:04 +0300

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    arr = []
+
+    for i in range(length):
+        arr.insert(len(arr), (arr[-1]) + (arr[-2]) if len(arr) > 1 else i)
+    print(arr)
+
+print_fibonacci(9)
+
+
